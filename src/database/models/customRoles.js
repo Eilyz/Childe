4c95e74397be324da03527906f218613e22a7faf
Zyla <erencana12@gmail.com>
2022-06-01 21:18:41 +0300

    Update
    Purge unnecessary code
    Add missing codes
    Update the overall bot

--- src/database/models/customRoles.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const CustomRolesSchema = new mongoose.Schema({
-  memberID: String,
-  roleID: String,
-})
-
-export default mongoose.model('customRoles', CustomRolesSchema)
--- src/database/models/modmails.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const ModmailsSchema = new mongoose.Schema({
-  memberID: String,
-  channelID: String,
-})
-
-export default mongoose.model('modmails', ModmailsSchema)
